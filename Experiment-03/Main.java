b2ea0f9e853e32bc8a9b2cf083caea9cd9f16b0b
Yogashri R <yogashri.rm1@gmail.com>
2026-09-24 05:17:36 +0530

    Add Experiment 03 - Student Admission Management System

---
+++ Experiment-03/Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter Student ID: ");
+        int studentId = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.print("Enter Student Name: ");
+        String studentName = scanner.nextLine();
+
+        System.out.print("Enter Department: ");
+        String department = scanner.nextLine();
+
+        System.out.print("Enter Age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter Percentage: ");
+        double percentage = scanner.nextDouble();
+
+        System.out.println("\nStudent Details");
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Department: " + department);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+
+        scanner.close();
+    }
+}
